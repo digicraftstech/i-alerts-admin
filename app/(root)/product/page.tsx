a735f888f4c14a3a1e382bdae6c4da912d530284
pallavisaxena <saxenapallavi20@gmail.com>
2026-04-10 16:14:04 +0530

    Add product button added , icon changed

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
 import ProductCard from '@/components/cards/ProductCard';
 
 import { ActionResponse, ItemArrayResponse, Product } from '@/types/global';
 import { getAllProducts } from '@/lib/actions/product.action';
+import { Button } from '@/components/ui/button';
+import { ROUTES } from '@/constants';
 
 const getProducts = async () => {
   const result = (await getAllProducts()) as ActionResponse<Product>;
   const data = result.data as ItemArrayResponse<Product>;
-  // console.log('proucts- dataArray: ', data.data);
+  // console.log('proucts- dataArray: ', data);
   return data.data as Product[];
 };
 
@@ -16,7 +19,12 @@ const Catalog = async () => {
   const products = await getProducts();
   return (
     <div>
-      <h1 className='h1-bold text-dark100_light900'>Products</h1>
+      <div className='flex flex-row justify-between'>
+        <h1 className='h1-bold text-dark100_light900'>Products</h1>
+        <Button asChild variant='outline' size='sm'>
+          <Link href={ROUTES.ADD_PRODUCT}>+ Add Product</Link>
+        </Button>
+      </div>
       <div className='mt-10 flex w-full flex-col gap-6'>
         {products && products.length ? (
           products.map((product: Product) => (
